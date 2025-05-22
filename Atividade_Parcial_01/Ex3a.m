1a3f79c46223727b56a77e48d53ca751ba9c8e02
Thiago M <rpj134@gmail.com>
2025-05-22 19:08:15 -0300

    Add resolução final da atividade parcial 1

--- Atividade_Parcial_01/Ex3a.m
+++ Atividade_Parcial_01/Ex3a.m
@@ -5,8 +5,8 @@ Gs = tf(200, [0.5 1 0]);
 red = 1/100;
 sensor = 0.07;
 
-ftma = series(Gso*K, Gs*red*sensor);
-ftmf = feedback(ftma, 1);
+ftma = series(Gso*K, Gs*red);
+ftmf = feedback(ftma, sensor)
 
 T = 0.1;
 G_z = c2d(ftmf, T, 'tustin');
